f33f30d6b5bcf1b297479670ae28f34db8835023
Mayank Raj <mayraj035@gmail.com>
2024-01-29 03:19:47 +0530

    User model update, fixed codable complaiance

--- ReflectAI/Models/User.swift
+++ ReflectAI/Models/User.swift
@@ -7,36 +7,44 @@
 
 import Foundation
 
-final class User: ObservableObject, Decodable, Encodable{
-    var id = UUID()
+final class User: ObservableObject, Codable{
+    var id: UUID
     var firstName: String
     var lastName: String
     var dob: Date
     @Published var journal: Journal
     
-    init(firstName: String, lastName: String, dob: Date, journal: Journal = Journal()) {
-        self.firstName = first_Name
-        self.lastName = last_Name
+    init(id: UUID = UUID(), firstName: String, lastName: String, dob: Date, journal: Journal = Journal()) {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
         self.dob = dob
         self.journal = journal
     }
+    
     private enum CodingKeys: String, CodingKey {
         case id
         case firstName
         case lastName
         case dob
+        case journal
     }
-
+    
     required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
-        firstName = try container.decode(String.self, forKey: .first_name)
-        lastName = try container.decode(String.self, forkey: .lastName)
-        
+        let containter = try decoder.container(keyedBy: CodingKeys.self)
+        id = try containter.decode(UUID.self, forKey: .id)
+        firstName = try containter.decode(String.self, forKey: .firstName)
+        lastName = try containter.decode(String.self, forKey: .lastName)
+        dob = try containter.decode(Date.self, forKey: .dob)
+        journal = try containter.decode(Journal.self, forKey: .journal)
     }
-
-    func encode(to encoder: Encoder) throws {
+    
+    func encode(to encoder: Encoder) throws{
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(entries, forKey: .entries)
+        try container.encode(id, forKey: .id)
+        try container.encode(firstName, forKey: .firstName)
+        try container.encode(lastName, forKey: .lastName)
+        try container.encode(dob, forKey: .dob)
+        try container.encode(journal, forKey: .journal)
     }
 }
