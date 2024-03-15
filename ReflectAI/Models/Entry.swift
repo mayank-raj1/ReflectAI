265a36ea666a00dd04af66a8e2ebcc0a983cb2f0
Mayank Raj <mayraj035@gmail.com>
2024-03-16 02:18:28 +0530

    Entry modifications

--- ReflectAI/Models/Entry.swift
+++ ReflectAI/Models/Entry.swift
@@ -8,43 +8,39 @@
 import Foundation
 import SwiftUI
 
-final class Entry: ObservableObject, Codable, Identifiable{
+struct Entry: Codable, Identifiable, Hashable{
     static func == (lhs: Entry, rhs: Entry) -> Bool {
-        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+        return lhs.id == rhs.id
     }
     
     let id: UUID
-    var date: Date
-    var heading: String
-    @Published var entry: String
+    var date: Date = Date.now
+    var entry: String = ""
     
-    init(id: UUID = UUID(), date: Date = Date(), entry: String = "", heading: String = "Morning Outage to meh") {
-        self.id = id
-        self.date = date
-        self.heading = heading
-        self.entry = entry
-    }
     
     private enum CodingKeys: String, CodingKey {
         case id
         case date
-        case heading
         case entry
     }
 
-    required init(from decoder: Decoder) throws {
+    init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
-        date = try container.decode(Date.self, forKey: .date)
-        heading = try container.decode(String.self, forKey: .heading)
-        entry = try container.decode(String.self, forKey: .entry)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.date = try container.decode(Date.self, forKey: .date)
+        self.entry = try container.decode(String.self, forKey: .entry)
     }
-
+    
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
         try container.encode(date, forKey: .date)
-        try container.encode(heading, forKey: .heading)
         try container.encode(entry, forKey: .entry)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(date)
+        hasher.combine(entry)
     }
 }
